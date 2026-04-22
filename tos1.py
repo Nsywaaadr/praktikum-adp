9df488e391e706894ecb534d0c747019f204c18b
Nsywaaadr <162964166+Nsywaaadr@users.noreply.github.com>
2026-04-22 10:25:44 +0700

    Update tos1.py

--- tos1.py
+++ tos1.py
@@ -5,65 +5,63 @@ FILE = "data_tugas.json"
 
 def load_data():
     if not os.path.exists(FILE):
-        return[]
+        return []
     with open(FILE, "r") as file:
-    return json.load(file)
+        return json.load(file)
 
 def save_data(data):
     with open(FILE, "w") as file:
-        jso.dump(data,file,indent=4)
+        json.dump(data, file, indent=4)
 
 def tambah_tugas(data):
-    nama = imput("Nama Tugas:")
-
-
-tugas = { "nama": nama,
-         }
-
-data.append(tugas)
-save_data(data)
-print("Tugas berhasil ditambahkan!")
+    nama = input("Nama Tugas: ")
+    tugas = {
+        "nama": nama
+    }
+    data.append(tugas)
+    save_data(data)
+    print("Tugas berhasil ditambahkan!")
 
 def tampilkan_tugas(data):
     if not data:
-        print("tidak ada tugas.")
+        print("Tidak ada tugas.")
         return
     
-print("\Daftar Tugas:")
-for i, t in enumerate(data,1):
-    print(f"{i}. {t['nama']}")
+    print("\nDaftar Tugas:")
+    for i, t in enumerate(data, 1):
+        print(f"{i}. {t['nama']}")
 
 def hapus_tugas(data):
     tampilkan_tugas(data)
     try:
-        index = int(input("Pilih nomor tugas yang ingin dihapus:"))
+        index = int(input("Pilih nomor tugas yang ingin dihapus: "))
         data.pop(index - 1)
         save_data(data)
-        print("tugas dihapus!")
+        print("Tugas dihapus!")
     except:
-        print("input salah!")
+        print("Input salah!")
 
 def menu():
     data = load_data()
 
-while True:
-    print("\n=== SISTEM INFORMASI TUGAS MAHASISWA ===")
-    print("1. Tambah tugas")
-    print("2. Lihat Tugas")
-    print("3. Hapus Tugs")
-    print("4. Keluar")
+    while True:
+        print("\n=== SISTEM INFORMASI TUGAS MAHASISWA ===")
+        print("1. Tambah tugas")
+        print("2. Lihat Tugas")
+        print("3. Hapus Tugas")
+        print("4. Keluar")
 
-    pilih = input("Pilih menu:")
+        pilih = input("Pilih menu: ")
 
-    if pilih == "1":
-        tambah_tugas(data)
-    elif pilih == "2":
-        tampilkan_tugas(data)
-    elif pilih == "3":
-        hapus_tugas(data)
-    elif pilih == "4":
-        break
-    else:
-        print("Pilihan tidak valid!")
+        if pilih == "1":
+            tambah_tugas(data)
+        elif pilih == "2":
+            tampilkan_tugas(data)
+        elif pilih == "3":
+            hapus_tugas(data)
+        elif pilih == "4":
+            break
+        else:
+            print("Pilihan tidak valid!")
 
 menu()
